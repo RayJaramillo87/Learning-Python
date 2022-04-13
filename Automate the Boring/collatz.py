b82dd521f04861ba33a2660ea3d6b159964b9ddc
RayJaramillo87 <jaramillo.ray@gmail.com>
2022-04-13 08:42:47 -0600

    Latest

---
+++ Automate the Boring/collatz.py
@@ -0,0 +1,19 @@
+def collatz(number):
+    if number % 2 == 0:
+        result = number // 2
+        print(result)
+        return result
+    elif number % 2 == 1:
+        result = 3 * number + 1
+        print(result)
+        return result
+
+
+print('Please insert a number: ')
+try:
+    number = int(input())
+except ValueError:
+    print('You must enter a whole Integer.')
+
+while number != 1:
+    number = collatz(int(number))
