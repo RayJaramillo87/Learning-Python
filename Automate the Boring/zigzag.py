22490e13325f1afa0ba23e3ebba5e9498bcbb3d2
RayJaramillo87 <jaramillo.ray@gmail.com>
2022-05-02 08:58:33 -0600

    Latest

--- Automate the Boring/zigzag.py
+++ Automate the Boring/zigzag.py
@@ -5,16 +5,16 @@ try:
     while True: # The main program loop.
         print(' ' * indent, end='')
         print('Leti Leti Leti Leti cant you see sometimes your words just hypnotize me!')
-        time.sleep(0.25) # Pause for 1/10 of a second.
+        time.sleep(0.01) # Pause for 1/10 of a second.
         if indentIncreasing:
             # Increase the number of spaces:
-            indent = indent + random.randint(1,5)
-            if indent >= 75:
+            indent = indent + random.randint(1,15)
+            if indent >= 240:
                 # Change direction:
                 indentIncreasing = False
         else:
             # Decrease the number of spaces:
-            indent = indent - random.randint(1,5)
+            indent = indent - random.randint(1,25)
             if indent <= 0:
                 # Change direction:
                 indentIncreasing = True
